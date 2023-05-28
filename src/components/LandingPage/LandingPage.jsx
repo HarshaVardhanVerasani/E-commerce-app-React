1fc32f370a0df8128bf86baec68aed34257356b8
Harsha <harshavardhan81795@gmail.com>
2023-05-28 19:18:04 +0530

    asds

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 const LandingPage = () => {
   return (
     <>
       <h1>This is Home page</h1>
       <p>Under construction</p>
+      <Link to="/product-list">
+        <button className="btn btn-warning">Show Products</button>
+      </Link>
     </>
   );
 };
